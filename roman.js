812e0d9038197727ed95cc53579c15150a7d148d
kellyky <kelpopko@gmail.com>
2021-11-01 13:44:36 -0500

    Add directions I missed in first commit

--- roman.js
+++ roman.js
@@ -1,2 +1,7 @@
+// From [https://exercism.org/tracks/javascript/exercises/roman-numerals/edit](https://exercism.org/tracks/javascript/exercises/roman-numerals/edit)
+// Directions: [https://exercism.org/tracks/javascript/exercises/roman-numerals/edit](https://exercism.org/tracks/javascript/exercises/roman-numerals/edit)
+
+
 const toRoman = (num) => {
+
 }
