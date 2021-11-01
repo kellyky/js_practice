fc07aa2cdb2429e16659608563ced0f5515550ec
kellyky <kelpopko@gmail.com>
2021-11-01 13:50:46 -0500

    Add working notes

--- roman.js
+++ roman.js
@@ -1,6 +1,16 @@
 // From [https://exercism.org/tracks/javascript/exercises/roman-numerals/edit](https://exercism.org/tracks/javascript/exercises/roman-numerals/edit)
 // Directions: [https://exercism.org/tracks/javascript/exercises/roman-numerals/edit](https://exercism.org/tracks/javascript/exercises/roman-numerals/edit)
 
+/*    NOTES 
+ *    
+ *    We know num % 1000 =  // tells us if a number is evenly divisible by 1000
+ *    We know (num - (num % 1000)/1000 will tell us how many "M"s as thousands to repeat
+ *    We'll need to concat an "M" for each thousand
+ *    We also know that we'll need to figure out what to do with the Remainder
+ *    We know that we can get that remainder with num % 1000
+ *    We'll need to declare a variable to keep track of the remainder as we deduct for each Numeral
+ *    We'll need a variable (I think) to hold onto the Roman Numeral being built
+*/
 
 const toRoman = (num) => {
 
