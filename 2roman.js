8cab34f418e900c039fa512e8ecf5ca21e180b03
kellyky <kelpopko@gmail.com>
2021-11-03 17:32:18 -0500

    Replace erronious D with X

---
+++ 2roman.js
@@ -0,0 +1,73 @@
+const toRoman = (num) => {
+  let romanNumeral = "";
+  let M = "", D = "", C = "", L = "", X = "", V = "", I = "";
+
+  // Thousands builder: M
+  M = (num - (num % 1000)) / 1000; 
+  M = 'M'.repeat(M); 
+  let remainder = num % 1000; 
+  console.log(remainder);
+
+  // 900, 1900, 2900
+  if (remainder >= 900 && remainder < 1000) {
+    M += "CM";
+  } 
+  
+  // 500s builder: D
+  if (remainder >= 500 && remainder < 1000) {
+    D = "D"; 
+  } 
+
+  remainder %= 500;
+
+  // 100s builder: C
+  C = (remainder - (remainder % 100)) / 100;
+  C = 'C'.repeat(C);
+
+  remainder %= 100;
+
+
+  remainder -= 900;
+
+  
+  // 50s builder: L
+  if (remainder >= 50 && remainder < 100) {
+    L = "L";
+  }
+
+  remainder %= 50;
+
+  // 10s builder: X 
+  X = (remainder - (remainder % 10)) / 10;
+  X = 'X'.repeat(X);
+
+  remainder %= 10;
+
+
+  // 5s builder: V
+  if (remainder >= 5 && remainder < 10) {
+    V = "V";
+  }
+
+  remainder %= 5;
+
+  // 1s builder: I
+
+  I = (remainder - (remainder % 1)) / 1;
+  I = 'I'.repeat(I);
+
+  remainder %= 1;
+  
+   
+  // temp - delete once things are working 
+  console.log("\n" + "Number entered: " + num); 
+  console.log("Remainder: " + remainder); 
+
+  // keepers
+  romanNumeral = M + D + C + L + X + V + I;
+  return romanNumeral;
+}
+
+console.log(toRoman(900));
+console.log(toRoman(1900));
+console.log(toRoman(2900));
--- roman.js
+++ roman.js
@@ -3,32 +3,48 @@ const toRoman = (num) => {
   let romanNumeral = "";
   let M = "", D = "", C = "", L = "", X = "", V = "", I = "";
 
-  // Thousands builder: M
+  // 1000s builder: M
   M = (num - (num % 1000)) / 1000; 
   M = 'M'.repeat(M); 
   let remainder = num % 1000; 
-  console.log("After M (1000s): " + remainder); // temp
+  // console.log("After M (1000s): " + remainder); // temp
 
   // 900, 1900, 2900
   if (remainder >= 900 && remainder < 1000) {
     remainder /= 1000;
     remainder = Math.floor(remainder);
-    console.log("After CM (900): " +  remainder); // temp
     M += "CM";
   } 
+  // console.log("After CM (900): " +  remainder); // temp
   
   // 500s builder: D
   if (remainder >= 500 && remainder < 1000) {
     D = "D"; 
   } 
   remainder %= 500;
-  console.log("After D (500): " + remainder);
+  // console.log("After D (500): " + remainder);
+
+  // 400, 1400, 2400
+  if (remainder >= 400 && remainder < 500) {
+    remainder /= 500;
+    remainder = Math.floor(remainder);
+    D += "CD";
+  } 
+  // console.log("After CD (400): " +  remainder); // temp
 
   // 100s builder: C
   C = (remainder - (remainder % 100)) / 100;
   C = 'C'.repeat(C);
   remainder %= 100;
-  console.log("After C (100s): " + remainder);
+  // console.log("After C (100s): " + remainder);
+  
+  // 90, 190, 290, etc.
+  if (remainder >= 90 && remainder < 100) {
+    remainder /= 100;
+    remainder = Math.floor(remainder);
+    C += "XC";
+  } 
+  // console.log("After XC (90): " +  remainder); // temp
   
   
   // 50s builder: L
@@ -36,22 +52,46 @@ const toRoman = (num) => {
     L = "L";
   }
   remainder %= 50;
-  console.log("After L (50): " + remainder); // temp
+  // console.log("After L (50): " + remainder); // temp
+
+  // 40, 140, etc. 
+  if (remainder >= 40 && remainder < 50) {
+    remainder /= 50;
+    remainder = Math.floor(remainder);
+    L += "XL";
+  } 
+  // console.log("After DL (40): " +  remainder); // temp
 
   // 10s builder: X 
   X = (remainder - (remainder % 10)) / 10;
   X = 'X'.repeat(X);
   remainder %= 10;
-  console.log("After X (10s): " + remainder); // temp
+  // console.log("After X (10s): " + remainder); // temp
 
+
+  // 9, 19, 29, etc.
+  if (remainder == 10) {
+    remainder /= 10;
+    remainder = Math.floor(remainder);
+    X += "IX";
+  } 
+  // console.log("After IX (9): " +  remainder); // temp
 
   // 5s builder: V
   if (remainder >= 5 && remainder < 10) {
     V = "V";
   }
   remainder %= 5;
-  console.log("After V (5): " + remainder); // temp
+  // console.log("After V (5): " + remainder); // temp
 
+
+  // 4, 14, etc. 
+  if (remainder == 4) {
+    remainder /= 5;
+    remainder = Math.floor(remainder);
+    V += "IV";
+  } 
+  // console.log("After IV (4): " +  remainder); // temp
 
   // 1s builder: I
 
@@ -68,6 +108,16 @@ const toRoman = (num) => {
   return romanNumeral;
 }
 
-console.log(toRoman(905));
-console.log(toRoman(1900));
-console.log(toRoman(2450));
+console.log(toRoman(4));
+console.log(toRoman(14));
+console.log(toRoman(44));
+console.log(toRoman(104));
+console.log(toRoman(144));
+console.log(toRoman(404));
+console.log(toRoman(444));
+console.log(toRoman(1004));
+console.log(toRoman(1014));
+console.log(toRoman(1214));
+console.log(toRoman(1040));
+console.log(toRoman(1044));
+console.log(toRoman(1444));
