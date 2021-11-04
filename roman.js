2d1a9d093205b251f9f5bfcc300d35facbe188ad
kellyky <kelpopko@gmail.com>
2021-11-03 20:04:10 -0500

    Remove a couple older lines of code and unnecessary filter method

--- roman.js
+++ roman.js
@@ -1,79 +1,70 @@
 const toRoman = (num) => {
   console.log("\n" + "Number entered: " + num); 
 
-  let M = "", D = "", C = "", L = "", X = "", V = "", I = "";
-  let romanNumeralArray = [];
-  
-
+  let romanNumeral = [];
 
   let repeatThisManyTimes = (num - (num % 1000)) / 1000; 
-  romanNumeralArray.push('M'.repeat(repeatThisManyTimes)); 
+  romanNumeral.push('M'.repeat(repeatThisManyTimes)); 
   let remainder = num % 1000; 
   
   if (remainder >= 900 && remainder < 1000) {
-    romanNumeralArray.push('CM'); 
+    romanNumeral.push('CM'); 
     remainder -= 900;
   } 
 
   if (remainder >= 500 && remainder < 1000) {
-    romanNumeralArray.push('D');
+    romanNumeral.push('D');
   } 
   remainder %= 500;
 
   if (remainder >= 400 && remainder < 500) {
-    romanNumeralArray.push('CD');
+    romanNumeral.push('CD');
     remainder -= 400;
   } 
 
   repeatThisManyTimes = (remainder - (remainder % 100)) / 100;
-  romanNumeralArray.push('C'.repeat(repeatThisManyTimes)); 
+  romanNumeral.push('C'.repeat(repeatThisManyTimes)); 
   remainder %= 100;
   
   if (remainder >= 90 && remainder < 100) {
-    romanNumeralArray.push('XC'); 
+    romanNumeral.push('XC'); 
     remainder -= 90;
   } 
   
   if (remainder >= 50 && remainder < 100) {
-    romanNumeralArray.push('L');
+    romanNumeral.push('L');
   }
   remainder %= 50;
 
   if (remainder >= 40 && remainder < 50) {
-    romanNumeralArray.push('XL');
+    romanNumeral.push('XL');
     remainder -= 40;
   } 
 
   repeatThisManyTimes = (remainder - (remainder % 10)) / 10;
-  romanNumeralArray.push('X'.repeat(repeatThisManyTimes)); 
+  romanNumeral.push('X'.repeat(repeatThisManyTimes)); 
   remainder %= 10;
 
 
   if (remainder == 9) {
-    romanNumeralArray.push('IX');
+    romanNumeral.push('IX');
     remainder -= 9;
   } 
 
   if (remainder >= 5 && remainder < 10) {
-    romanNumeralArray.push('V');
+    romanNumeral.push('V');
   }
   remainder %= 5;
 
   if (remainder == 4) {
-    romanNumeralArray.push('IV');
+    romanNumeral.push('IV');
     remainder -= 4;
   } 
 
   repeatThisManyTimes = (remainder - (remainder % 1)) / 1;
-  romanNumeralArray.push('I'.repeat(repeatThisManyTimes)); 
+  romanNumeral.push('I'.repeat(repeatThisManyTimes)); 
   
-  
-  const removeEmpty = romanNumeralArray.filter(el => {
-    return el != null;
-  });
-
-  let romanNumeral = removeEmpty.join('');
-  return romanNumeral;
+  return romanNumeral.join('');
 
 }
 
