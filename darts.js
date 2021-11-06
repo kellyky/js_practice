83485f81dab414188c9453368593ae56e217ac6a
kellyky <kelpopko@gmail.com>
2021-11-06 18:37:07 -0500

    Add darts.js exercize

---
+++ darts.js
@@ -0,0 +1,12 @@
+// From darts exercise - exercism https://exercism.org/tracks/javascript/exercises/darts/
+
+// In our particular instance of the game, the target rewards with 4 different amounts of points, depending on where the dart lands:
+
+// If the dart lands outside the target, player earns no points (0 points).
+// If the dart lands in the outer circle of the target, player earns 1 point.
+// If the dart lands in the middle circle of the target, player earns 5 points.
+// If the dart lands in the inner circle of the target, player earns 10 points.
+
+const score = () => {
+
+}
