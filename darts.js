6697e9baeda2b766ecdd9d2143aee7ac16cda689
kellyky <kelpopko@gmail.com>
2021-11-06 18:41:02 -0500

    Add pseudocode skeleton of exercise

--- darts.js
+++ darts.js
@@ -1,12 +1,17 @@
 // From darts exercise - exercism https://exercism.org/tracks/javascript/exercises/darts/
 
-// In our particular instance of the game, the target rewards with 4 different amounts of points, depending on where the dart lands:
-
-// If the dart lands outside the target, player earns no points (0 points).
-// If the dart lands in the outer circle of the target, player earns 1 point.
-// If the dart lands in the middle circle of the target, player earns 5 points.
-// If the dart lands in the inner circle of the target, player earns 10 points.
 
 const score = () => {
+  let points;
+  if (/*inner circle*/) {
+    // 10 points
+  } else if (/* middle circle */) {
+    // 5 points
+  } else if (/* outer circle */) {
+    // 1 point
+  } else {
+    // 0 points
+  }
+  return points;
 
 }
