bdceb713b13088c27ebe99e0bd8e82630cf22dc9
kellyky <kelpopko@gmail.com>
2021-11-06 19:30:30 -0500

    Add working function :-)

--- darts.js
+++ darts.js
@@ -3,7 +3,9 @@
 
 const score = (x, y) => {
   let points;
-  let radius;
+  let radius = ((x ** 2) + (y ** 2)) ** (0.5);
+  // I *think* we 
+
 
   if (radius < 1) {
     points = 10;
@@ -14,10 +16,12 @@ const score = (x, y) => {
   } else {
     points = 0;
   }
+  console.log(`Coords entered: ${x}, ${y}`);
   return points;
 
 }
 
-console.log(score(1, 15));
+console.log(score(-0.1, -0.1 ));
 console.log(score(1, 1));
 console.log(score(5, 5));
+console.log(score(9, -9));
