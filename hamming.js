42a14d29b9be4dcf6bb029c5e9f6f5daa166c01e
kellyky <kelpopko@gmail.com>
2021-11-07 22:31:23 -0600

    hamming.js: Add error if strings different lengths

--- hamming.js
+++ hamming.js
@@ -1,6 +1,9 @@
 // From Exercism: https://exercism.org/tracks/javascript/exercises/hamming
 
 const compute = (dna, dna2) => {
+  if (dna.length != dna2.length) {
+    throw new Error("strands must be of equal length")
+  }
 
     return hammingDistance;
 };
