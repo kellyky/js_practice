af66decebc08acfb7b1aaa3a9fab6c76668ba38a
kellyky <kelpopko@gmail.com>
2021-11-07 22:47:45 -0600

    hamming.js: Complete first working iteration

--- hamming.js
+++ hamming.js
@@ -2,8 +2,22 @@
 
 const compute = (dna, dna2) => {
   if (dna.length != dna2.length) {
-    throw new Error("strands must be of equal length")
+    return "strands must be of equal length";
   }
 
-    return hammingDistance;
-};
+  let hammingDistance = 0;
+
+  let index = 0;
+  while ( index < dna.length ) {
+    if (dna.charAt(index) != dna2.charAt(index)) {
+      hammingDistance++;
+    }
+    index++;
+  }
+
+  return hammingDistance;
+}
+
+// console.log(compute('AATG', 'AAA'));
+// console.log(compute('ggactgaaatctg', 'ggactgaaatctg'));
+console.log(compute('GGACGGATTCTG', 'AGGACGGATTCT'));
