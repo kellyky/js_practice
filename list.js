c759cd54a177e50b7cf793db527a730b67705c38
kellyky <kelpopko@gmail.com>
2021-11-09 00:15:28 -0600

    list.js: Add loop to build layers

--- list.js
+++ list.js
@@ -3,3 +3,18 @@
   // Chapter Notes: https://free-guanaco-140.notion.site/Chapter-4-Data-Structures-Objects-and-Arrays-2825c6d759084545805b329a391f221a
   // Exercise Notes: https://free-guanaco-140.notion.site/Chapter-4-Exercises-def48d0cd53448458deb49e8cdf736e2
 
+
+const arrayToList = (array) => {
+  let list = {};
+  for (let item in array) {
+    list = {
+      value: null,
+      rest: list,
+    }
+  };
+  return list;
+  }
+
+
+console.log(arrayToList([1, 2, 3]));
+
