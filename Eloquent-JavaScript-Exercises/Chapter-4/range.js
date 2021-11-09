deaf5038fcf7b5fb0873457ff4d960da1c85b09b
kellyky <kelpopko@gmail.com>
2021-11-09 00:51:21 -0600

    range.js: Add new version that adds step arg

--- Eloquent-JavaScript-Exercises/Chapter-4/range.js
+++ Eloquent-JavaScript-Exercises/Chapter-4/range.js
@@ -4,27 +4,47 @@
   // Exercise Notes: https://free-guanaco-140.notion.site/Chapter-4-Exercises-def48d0cd53448458deb49e8cdf736e2
 
 
-function range(start, end){
-  let array=[];
+// // Version 1 of exercise
+// function range(start, end){
+//   let array=[];
+//   if (start == end) {
+//     array.push(start);
+//   } else {
+//     if (start < end) {
+//       for (i = start; i <= end; i++) {
+//         array.push(i);
+//       }
+//     } else {
+//       for (i = start; i >= end; i--) {
+//         array.push(i);
+//       }
+//     }
+//   }
+//   return array;
+// }
+
+
+function range(start, end, step=1){
+let array=[];
   if (start == end) {
     array.push(start);
   } else {
     if (start < end) {
-      for (i = start; i <= end; i++) {
+      for (i = start; i <= end; i = i+step) {
         array.push(i);
       }
     } else {
-      for (i = start; i >= end; i--) {
+      for (i = start; i >= end; i=i-step) {
         array.push(i);
       }
     }
   }
-  return array;
+      return array;
 }
 
 console.log(range(1, 3));
 // [1, 2, 3]
-console.log(range(2, -2));
-// [2, 1, 0, -1, -2]
-console.log(range(10, 10));
-// [10]
+console.log(range(2, -2, 2));
+// [2, 0, -2]
+console.log(range(10, 30, 5));
+// [10, 15, 20, 25, 30]
