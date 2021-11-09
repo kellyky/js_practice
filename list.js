043f3bd310e3e0ebe9bcd25f9498c3ee2aec10d0
kellyky <kelpopko@gmail.com>
2021-11-08 21:58:23 -0600

    list.js: Add list.js file

---
+++ list.js
@@ -0,0 +1,5 @@
+// From Eloquent Javascript Chapter 4: https://eloquentjavascript.net/04_data.html
+// Chapter 4 
+  // Chapter Notes: https://free-guanaco-140.notion.site/Chapter-4-Data-Structures-Objects-and-Arrays-2825c6d759084545805b329a391f221a
+  // Exercise Notes: https://free-guanaco-140.notion.site/Chapter-4-Exercises-def48d0cd53448458deb49e8cdf736e2
+
