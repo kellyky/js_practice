36f75d80685eee4c9997c4be3fd39eaf0ea8a45c
kellyky <kelpopko@gmail.com>
2021-12-14 18:59:57 -0600

    Correctly outputs to new array by index position

--- array_methods/verticleArrMaker.js
+++ array_methods/verticleArrMaker.js
@@ -1,38 +1,31 @@
+// Goal: From that array, create an array of 4 arrays ['AAAA', 'BBBB', 'CCCC', 'DDDD']
+
 // The Starting array
 const ABCD = ['ABCD', 'ABCD', 'ABCD', 'ABCD'];
 console.log(ABCD);
 
-const eachIndex = ABCD.map((el, index) => index); 
+// Creates new array of indexes of each off array ABCD
+const indices = ABCD.map((el, index) => index); 
+console.log(indices);
 
-// const indArr = (ABCD).map((el, index) => index);
-console.log(eachIndex);
+function arrayOfLetter (array, index = 0) {
+  let theNewArr = [];
+  let letterArr = [];
 
+  while (index < array.length){
+    for (let el of array) {
+      letterArr.push(el[index]);
+    }
+    index++;
+    }
+    const entry = letterArr.join('');
+    theNewArr.push(entry);
+  return theNewArr;
 
-// console.log(indArr(someArr));
-
-
-
-
-// Goal: From that array, create an array of 4 arrays ['AAAA', 'BBBB', 'CCCC', 'DDDD']
-
-
-// function arrFilter (ABCD, [...indexArr]) {
-//   let filteredArr = [];
-//   for (let el of array) {
-//     filteredArr.push(el[indexArr]); 
-//   }
-//   return filteredArr;
-
-
-
-// }
-
-// console.log(arrFilter(ABCD));
+}
 
 
 
+console.log(arrayOfLetter(ABCD, ...indices));
 
 
-
-// console.log('\n');
-// console.log('\n');
