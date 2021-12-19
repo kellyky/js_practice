2453ec421bfdfdf50985d30c820d937a2b2e090f
kellyky <kelpopko@gmail.com>
2021-12-19 00:31:27 -0600

    Start of part 2: it iterates and correctly returns entries with 'b' in correct position; still pulls from whole array though

--- array_methods/someArrMethod.js
+++ array_methods/someArrMethod.js
@@ -10,8 +10,8 @@
 const theArray = ['aaa', 'aab', 'aba', 'baa', 'abb', 'bbb', 'bba', 'bab'] 
 console.log(theArray);
 
-// Part 1:
 
+// Part 1:
 let newArr = theArray.filter(el => el[0] == 'a');
 
 console.log('index 0 winners');
@@ -27,3 +27,15 @@ console.log('index 2 winners');
 console.log(newestArr);
 
 
+// Part 2: Iterative version of part 1, use b so I can keep what I have up
+
+
+const iterative = arr => {
+  let tempArr = [];
+  for (let i in arr) {
+    tempArr.push(arr.filter(el => el[i] == 'b'));
+  }
+  return tempArr;
+}
+
+console.log(iterative(theArray));
