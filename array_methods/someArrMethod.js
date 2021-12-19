9a2150bc4348912d67a957197172e997479b372b
kellyky <kelpopko@gmail.com>
2021-12-19 00:06:19 -0600

    Upload new exercise

---
+++ array_methods/someArrMethod.js
@@ -0,0 +1,22 @@
+// Part 1:
+// Given an array, return only entries with a in each index
+//
+// Part 2:
+// Refactor to iterate through 
+//
+// Part 3:
+// Refactor to filter by which letter is more prevalent in each index
+
+const theArray = ['aaa', 'aab', 'aba', 'baa', 'abb', 'bbb', 'bba', 'bab'] 
+console.log(theArray);
+
+// Part 1:
+
+console.log('index 0 winners');
+console.log(theArray.filter(el => el[0] == 'a'));
+console.log('index 1 winners');
+console.log(theArray.filter(el => el[1] == 'a'));
+console.log('index 2 winners');
+console.log(theArray.filter(el => el[2] == 'a'));
+
+
