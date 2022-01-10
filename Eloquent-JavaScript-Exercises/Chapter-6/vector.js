00a977f9b82d3060c2845255e8bbf0ac32c06b9e
kellyky <kelpopko@gmail.com>
2022-01-10 15:55:10 -0600

    Adds directions and file

---
+++ Eloquent-JavaScript-Exercises/Chapter-6/vector.js
@@ -0,0 +1,19 @@
+/*
+ *
+ Write a class Vec that represents a vector in 2 space
+ - Takes x and y params; these are numbers
+ - saves to properties of the same name
+ Give Vec 2 methods:
+ - plus
+ - minus
+ These should: 
+  - take another vector as a param  
+  - return a new vector that has the sum or difference of the two vectors' (this and the parameter) x and y vals
+  Add getter property length to prodotype
+  Should compute the length of vector - distance of point (x, y) and origin (0, 0)
+
+*/
+
+class Vec {
+
+}
