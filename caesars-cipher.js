163a12b0a3cac69a6c58a8d0d60a68ce76d703f9
Kelly Young <kelpopko@gmail.com>
2022-02-15 13:54:53 -0600

    New approach

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,41 +1,23 @@
-const CHARACTER_CHART = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-}
+const ROTATION = 13;
 
 function rot13(str) {
   let decodedStr = "";
 
   for (let i = 0; i < str.length; i++){
-    if (/[A-Z]/.test(str[i])){
-      decodedStr += CHARACTER_CHART[str[i]];
-    }
-    else {
-      decodedStr += str[i];
+    const char = str[i];  // original char
+    const charCode = str.charCodeAt(i);   // charCode for char
+
+    // if char is a letter, decode char
+    if(/[A-M]/.test(char)){
+      const decodedChar = charCode + ROTATION;
+      decodedStr += String.fromCharCode(decodedChar);
+
+    } else if (/[N-Z]/.test(char)){
+      const decodedChar = charCode - ROTATION;
+      decodedStr += String.fromCharCode(decodedChar);
+
+    } else {
+      decodedStr += char;
     }
     
   }
