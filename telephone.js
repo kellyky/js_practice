61af0a127c22d4a65497aeb790f898216bef8dc9
Kelly Young <kelpopko@gmail.com>
2022-02-19 22:48:49 -0600

    Add chonky regexes

--- telephone.js
+++ telephone.js
@@ -1,7 +1,31 @@
-// starter code:
+const RE = /^\d{3}-\d{3}-\d{4}$/;
+const RE2 = /^\(\d{3}\)\s?\d{3}-\d{4}$/;
+const RE3 = /^\d{3}\s?\d{3}\s?\d{4}$/;
+const RE4 = /^1\s\d{3}\s\d{3}\s\d{4}$/;
+const RE5 = /^1\s\d{3}-\d{3}-\d{4}$/;
+const RE6 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}$/;
+
+
+// Create array of regexes
+const REGEXES = [RE, RE2, RE3, RE4, RE5, RE6];
 
 function telephoneCheck(str) {
-  return true;
+  // loop through regexes
+  for (let regex of REGEXES){
+  
+    // if one matches, return true
+    if (regex.test(str)){
+      return true;
+    }
+  } 
+  return false;
 }
 
-telephoneCheck("555-555-5555");
+
+console.log(telephoneCheck("1 456 789 4444")); //true
+console.log(telephoneCheck("1 (555) 555-5555")); //true
+console.log(telephoneCheck("555 555 5555")); //true
+console.log(telephoneCheck("(555)5(55?)-5555")); //false
+console.log(telephoneCheck("1 555 555 5555")); //true
+console.log(telephoneCheck("1(555)555-5555")); //true
+console.log(telephoneCheck("555-555-5555")); // true
