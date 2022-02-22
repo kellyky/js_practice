42012b6a7198fde291ac250a91df46363a770204
Kelly Young <kelpopko@gmail.com>
2022-02-21 18:56:17 -0600

    Clean up comments

--- algo-practice.js/mutations.js
+++ algo-practice.js/mutations.js
@@ -1,14 +1,7 @@
 function mutations(arr) {
-  // Assign variables for both elements in array & make them lowercase
   const [firstEl, secondEl] = arr.map(el => el.toLowerCase());
 
-  // loop through the letters of the second element "secondEl"
   for (let letter of secondEl){
-    // check to see if the first element "firstEl" contains the given letter from the second element 
-    
-    // console.log(firstEl.includes(letter));
-    // ^ logs true/false for each letter in the loop
-    // Checking for false's 
     if (!firstEl.includes(letter)){
       return false;
     }
