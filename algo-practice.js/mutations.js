06fa3b00c9d14488eb258fc196c34eb9a9cb479b
Kelly Young <kelpopko@gmail.com>
2022-02-21 22:10:49 -0600

    Refactor to use array methods

--- algo-practice.js/mutations.js
+++ algo-practice.js/mutations.js
@@ -1,15 +1,14 @@
-function mutations(arr) {
+function mutation(arr) {
   const [firstEl, secondEl] = arr.map(el => el.toLowerCase());
 
-  for (let letter of secondEl){
-    if (!firstEl.includes(letter)){
-      return false;
-    }
-  }
-  return true;
+  const findFalse = secondEl
+    .split('')
+    .map(letter => !firstEl.includes(letter))
+    .every(el => el === false);
+  return findFalse;  // returns false if there is no match
 }
 
-console.log(mutations(["hello", "Hello"])); // true
-console.log(mutations(["hello", "hey"])); // false
-console.log(mutations(["Mary", "Army"])); // true
-console.log(mutations(["Mary", "Aarmy"])); // true
+console.log(mutation(["hello", "Hello"])); // true
+console.log(mutation(["hello", "hey"])); // false
+console.log(mutation(["Mary", "Army"])); // true
+console.log(mutation(["Mary", "Aarmy"])); // true
