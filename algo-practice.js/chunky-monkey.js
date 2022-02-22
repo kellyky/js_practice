01f4b2856a6dd2feedc65c430cede34431fbf420
Kelly Young <kelpopko@gmail.com>
2022-02-21 19:14:41 -0600

    Add loop

--- algo-practice.js/chunky-monkey.js
+++ algo-practice.js/chunky-monkey.js
@@ -1,5 +1,10 @@
 function chunkArrayInGroups(arr, size){
+  console.log(arr);
+
+  for (let i = 0; i < size; i++){
+    console.log("x");
+  }
 
 }
 
-console.log(chunkArrayInGroups({"a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
