a4307956b7c872fa6796c5bdfa027b6b6f25c5f8
Kelly Young <kelpopko@gmail.com>
2022-02-22 11:50:35 -0600

    Closer? Not there yet

--- algo-practice.js/chunky-monkey.js
+++ algo-practice.js/chunky-monkey.js
@@ -1,13 +1,22 @@
-function chunkArrayInGroups(arr, size){
-  // Logging to console, since I'm not sure where to start
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  
+  for (let i = 0; i < arr.length; i++){
+    let row = [];
+    arr.slice();
+    for (let j = 0; j < size; j++){
+      
+      row.push(arr[i]);
+    }
+     
 
-  console.log(arr);
-  console.log(arr.length);
-  console.log(size);
-  console.log(Math.ceil(arr.length / size));
-  console.log(arr.length % size);
+    // row.push(arr[i]);
+    newArr.push(row);
+    
+  }
+  
+  console.log(newArr);
 
 }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));   // should return [["a", "b"], ["c", "d"]]
-console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));  // [[0, 1, 2], [3, 4, 5], [6]]
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
