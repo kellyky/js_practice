0071d02eed0e465f509791a9f312661e18d74a8e
Kelly Young <kelpopko@gmail.com>
2022-02-21 19:27:58 -0600

    Add logging to gather data for starting point

--- algo-practice.js/chunky-monkey.js
+++ algo-practice.js/chunky-monkey.js
@@ -1,10 +1,13 @@
 function chunkArrayInGroups(arr, size){
-  console.log(arr);
+  // Logging to console, since I'm not sure where to start
 
-  for (let i = 0; i < size; i++){
-    console.log("x");
-  }
+  console.log(arr);
+  console.log(arr.length);
+  console.log(size);
+  console.log(Math.ceil(arr.length / size));
+  console.log(arr.length % size);
 
 }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));   // should return [["a", "b"], ["c", "d"]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));  // [[0, 1, 2], [3, 4, 5], [6]]
