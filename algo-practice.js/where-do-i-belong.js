1891e1a9395cbff56377aea8c8ee7ff4e150bd18
Kelly Young <kelpopko@gmail.com>
2022-02-27 20:56:25 -0600

    Address case where num is greater than all array values

--- algo-practice.js/where-do-i-belong.js
+++ algo-practice.js/where-do-i-belong.js
@@ -1,23 +1,23 @@
 function getIndexToIns(arr, num) {
+  
   const sortedArray = arr.sort((firstEl, secondEl) => firstEl - secondEl);
-  console.log(sortedArray);
 
-   if (sortedArray[0] == undefined){
+  if (sortedArray[0] == undefined){
       return 0;
-    }
-
+  } 
+  
   for (let i = 0; i < sortedArray.length; i++){
-
     if (sortedArray[i] >= num){
-      return i;
+      return i; 
     }
-   
   }
+
+  return sortedArray.length;
   
 }
 
 
-// console.log(getIndexToIns([40, 60], 50));
+
 console.log(getIndexToIns([5, 3, 20, 3], 5));
-console.log(getIndexToIns([2, 20, 10], 19));
-console.log(getIndexToIns([], 1));
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30))
+console.log(getIndexToIns([2, 5, 10], 15));
