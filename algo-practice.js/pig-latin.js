766c0f662966a1382cb2cbc1481ab8344e3edee4
Kelly Young <kelpopko@gmail.com>
2022-02-28 11:24:01 -0600

    Add pig-latin.js exercise

---
+++ algo-practice.js/pig-latin.js
@@ -0,0 +1,19 @@
+// From JavaScript Algorithms and Data Structures >> Intermediate Algorithm Scripting
+// Exercise link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/pig-latin
+// 
+// Description: Pig Latin is a way of altering English Words. The rules are as follows:
+  // - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+  // - If a word begins with a vowel, just add way at the end.
+
+
+function translatePigLatin(str) {
+  //
+  return str;
+}
+
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("california"));
+console.log(translatePigLatin("paragraphs"));
+console.log(translatePigLatin("eight"));
+console.log(translatePigLatin("schwartz"));
+console.log(translatePigLatin("rhythm"));
