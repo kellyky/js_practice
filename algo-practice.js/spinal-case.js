f4540820cd7100c7926526b735901d72f349aeaa
Kelly Young <kelpopko@gmail.com>
2022-02-27 22:15:01 -0600

    Add ability to handle caps/no spaces but broke what was working

--- algo-practice.js/spinal-case.js
+++ algo-practice.js/spinal-case.js
@@ -2,9 +2,28 @@
 // Directions: Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-  return str.toLowerCase().split(" ").join('-');
+  // let newString = str.split(" ");
+  
+  let reverseThis = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    const letter = str[i];
+    const capitalLetter = /[A-Z]/;
+
+    if (capitalLetter.test(letter)){
+      reverseThis.push(...[letter, " "]); 
+
+    } else {
+      reverseThis.push(letter);
+    }
+  }
+  const reversedArr = reverseThis.reverse().join("");
+  console.log(reversedArr);
+
+
+  // return str.toLowerCase().split(" ").join('-');
 }
 
-console.log(spinalCase('This Is Spinal Tap'));
-console.log(spinalCase()"The_Andy_Griffith_Show"));
-console.log(spinalCase("thisIsSpinalTap"))
+// console.log(spinalCase('This Is Spinal Tap'));
+// console.log(spinalCase("The_Andy_Griffith_Show"));
+console.log(spinalCase("thisIsSpinalTap"));
+// console.log(spinalCase("myCar"));
