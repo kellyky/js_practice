bbee10074b7106aee177c38369e2b7e3947929d6
Kelly Young <kelpopko@gmail.com>
2022-02-27 20:49:59 -0600

    Address empty array

--- algo-practice.js/where-do-i-belong.js
+++ algo-practice.js/where-do-i-belong.js
@@ -2,9 +2,13 @@ function getIndexToIns(arr, num) {
   const sortedArray = arr.sort((firstEl, secondEl) => firstEl - secondEl);
   console.log(sortedArray);
 
+   if (sortedArray[0] == undefined){
+      return 0;
+    }
+
   for (let i = 0; i < sortedArray.length; i++){
 
-    if (sortedArray[i] < num){
+    if (sortedArray[i] >= num){
       return i;
     }
    
@@ -16,3 +20,4 @@ function getIndexToIns(arr, num) {
 // console.log(getIndexToIns([40, 60], 50));
 console.log(getIndexToIns([5, 3, 20, 3], 5));
 console.log(getIndexToIns([2, 20, 10], 19));
+console.log(getIndexToIns([], 1));
