e2dcbc20f40c33d4523305f2079657bb6d36a81f
Kelly Young <kelpopko@gmail.com>
2022-02-28 20:13:19 -0600

    Rename variables, simplify code a bit

--- algo-practice.js/pig-latin.js
+++ algo-practice.js/pig-latin.js
@@ -3,41 +3,29 @@
 
 
 function translatePigLatin(str) {
-  const vowels = ["a", "e", "i", "o", "u"];
-  let localStr = str;
-  const localStrArr = str.split('');
+  let localStr = str; 
+  const localStrArr = str.split('');  
+  const vowels = ["a", "e", "i", "o", "u"];    
 
-  const firstLetter = localStr.split('').shift()
-  // console.log(firstLetter);
+  const hasVowels = localStrArr.filter(char => vowels.includes(char)).length > 0;
+  const firstLetterVowel = vowels.includes(localStr[0]);
 
-  const hasVowels = localStrArr.filter(letter => vowels.includes(letter)).length > 0; // returns T/F
-  // define consonant cluster at start of word
-
-
-
-// if hasVowels, check for vowel at wordstart
   if (hasVowels){
-    if (vowels.includes(localStr[0])){
-      localStr += "w";
-    } else {
-    let consCluster = "";
-    for (let i = 0; !vowels.includes(localStr[i]); i++){
-      consCluster += localStrArr[i];
+    if (firstLetterVowel) localStr += "w";
+    else if (!firstLetterVowel) {
+      let consCluster = "";
+      for (let i = 0; !vowels.includes(localStr[i]); i++){
+        consCluster += localStrArr[i];
+      }
+      localStr = localStrArr.slice(consCluster.length).join('') + consCluster; 
     }
-      const mainWord = localStrArr.slice(consCluster.length).join('') + consCluster;
-      localStr = mainWord;
-    } 
-  }
-
-  
+  } 
 
   return localStr + "ay";
-
 }
-
-
-// console.log(translatePigLatin("consonant"));
-// console.log(translatePigLatin("eight"));
-// console.log(translatePigLatin("schwartz"));
+  
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("eight"));
+console.log(translatePigLatin("schwartz"));
 console.log(translatePigLatin("rhythm"));
 
